e21b98c207b38b30d4506cd8265ff61322ec964d
Hiyawnew Haile <hiyawnewhaile@gmail.com>
2022-04-19 17:09:22 -0400

    plotting out blocks

---
+++ W1/D2/algo.js
@@ -0,0 +1,26 @@
+//variables
+var x = 25;
+
+console.log(x);
+
+console.log(x+x);
+
+var myname = ("Hiyaw");
+
+console.log(myname+" "+x);
+
+//loops
+
+// for(var i = 0;i < 10; i++){
+    // console.log(i);
+// }
+
+//arrays
+var myarr = [1,2,'x',x];
+
+console.log(myarr[2])
+
+
+for(var i = 0; i < myarr.length; i++){
+    console.log(myarr[i])
+}
