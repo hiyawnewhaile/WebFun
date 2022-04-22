b5f537159454ea617978094babb9489ff3b48b8e
Hiyawnew Haile <hiyawnewhaile@gmail.com>
2022-04-22 16:51:24 -0400

    fizzbuzz

---
+++ W1/D5/Lecture/script.js
@@ -0,0 +1,93 @@
+// //varibles
+// var name = 'bob'
+// var mynum = 8
+// var arr =[]
+// var mycat = {}
+// var israining = false
+
+// // loosely typed(easly changed)
+// //mynum='five'
+
+// //operators: + - / % && || ++ -- < > == === = != <= >=
+
+// //if statements
+// if(mynum > 10){
+//     //code
+//     console.log('this is an error')
+// }
+
+// //optionaly else or else if
+
+// // if(my num == 'four'){
+// //     console.log('i like 4')
+// // }else{
+// //     console.log('i wish it were four')
+// // }
+
+// // if(mynum == 'five' || is raining){
+// //     console.log('its a dreary day')
+// // }
+
+// //loops
+
+// for(var i=0; i<10; i++){
+//     console.log(i)
+// }
+
+// var i=0
+// while(i<10){
+//     console.log(i)
+//     i+=1
+// }
+
+// //arrays
+// //use array notation []
+// var myarrr = [a,b,c]
+
+// //get value at index 1
+// //arrays are 0 indexed that just means they start at 0
+// //get value at index1
+// myarr[1]
+// //set value at index 2
+// marr[2]= 'cat'
+
+// //we can store anythimg in an array 
+// //even null
+
+// myarr[0] = null;
+
+
+// //we can add to the end of an array with push
+// myarr.push('zebra')
+// console.log(myarr)
+
+// //we can also remove the last item with pop
+// myarr.pop()
+
+// //you can still push an element in a later index even if the later index is way more than the number of items currently in an array
+// myarr[9] = 100
+
+// //functions
+// //declare
+// funcrion setx(value, value3){
+//     x = value
+// }
+
+// //call
+// setx(4)
+
+// function makearr(num,num2,num3){
+//     return[num,num2,num3]
+// }
+
+// var newarr = makearr(1,2,3)
+// console.log(newarr)
+
+
+function changecolor(item){
+    item.style.backgroundColor='grey'
+}
+
+function changecolor2(color){
+    color.style.backgroundColor='white'
+}
