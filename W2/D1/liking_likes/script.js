1c37bcafa35277bd5e7a2d6499eaf7bee697262b
Hiyawnew Haile <hiyawnewhaile@gmail.com>
2022-04-26 14:56:30 -0400

    likinglikesupdate

--- W2/D1/liking_likes/script.js
+++ W2/D1/liking_likes/script.js
@@ -1,6 +1,6 @@
 var a = 9
 var aElement=document.querySelector('#a')
-function addlike(element){
+function addlike(){
     a++;
     aElement.innerHTML=a+" Like(s)";
     console.log(a);
@@ -8,7 +8,7 @@ function addlike(element){
 
 var b = 12
 var bElement=document.querySelector('#b')
-function addlike2(element){
+function addlike2(){
     b++;
     bElement.innerHTML=b+" Like(s)";
     console.log(b);
@@ -16,7 +16,7 @@ function addlike2(element){
 
 var c = 9
 var cElement=document.querySelector('#c')
-function addlike3(element){
+function addlike3(){
     c++;
     cElement.innerHTML=c+" Like(s)";
     console.log(c);
