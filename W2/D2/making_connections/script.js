ef2f1617ee2a0d9d02b055470f9ddf513703d48b
Hiyawnew Haile <hiyawnewhaile@gmail.com>
2022-04-26 18:08:47 -0400

    makingconnectionsupdt

--- W2/D2/making_connections/script.js
+++ W2/D2/making_connections/script.js
@@ -3,17 +3,26 @@ function changename(){
     name.innerText="Janet Borne"
 }
 
-// function disappears(){
-//     var request = document.querySelector("#a1")
-//     request.remove()
-// }
-
-// function disappears2(){
-//     var request = document.querySelector("#a2")
-//     request.remove()
-// }
-
-function masterdisappears(element){
-    var request = document.querySelector(element)
-    request.remove()
+function disappears(element){
+    var request = document.querySelector(element);
+    var reqs = parseInt(document.querySelector(".number2").innerText);
+    var frnds = parseInt(document.querySelector("#frnds").innerText);
+    request.remove();
+    reqs--;
+    document.querySelector(".number2").innerText=reqs;
+    frnds++;
+    document.querySelector("#frnds").innerText=frnds;
 }
+
+function disappears2(element){
+    var request = document.querySelector(element);
+    var reqs = parseInt(document.querySelector(".number2").innerText);
+    request.remove();
+    reqs--;
+    document.querySelector(".number2").innerText=reqs;
+}
+
+// function masterdisappears(element){
+//     var request = document.querySelector(element)
+//     request.remove()
+// }
