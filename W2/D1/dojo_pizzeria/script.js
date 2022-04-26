30c26bf77112ea8e738cb47bf10034ba87ec42ac
Hiyawnew Haile <hiyawnewhaile@gmail.com>
2022-04-26 00:42:05 -0400

    pizza_oven

---
+++ W2/D1/dojo_pizzeria/script.js
@@ -0,0 +1,20 @@
+function pizzaOven(crustType, sauceType, cheeses, toppings) {
+    var pizza = {};
+    pizza.crustType = crustType;
+    pizza.sauceType = sauceType;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+    
+var p1 = pizzaOven("deep dish", "traditional", "Mozzarella", ["pepperoni", "sausage"]);
+console.log(p1);
+
+var p2 = pizzaOven("hand tossed","marinara",["mozzarella","feta"],["mushrooms","olives","onions"]);
+console.log(p2);
+
+var p3 = pizzaOven("thin crust","alfredo",["mozzarella","parmesan"],["olives","beef","onions"])
+console.log(p3);
+
+var p4 = pizzaOven("stuffed crust","pesto",["ricotta","mozzarella"],"basil");
+console.log(p4)
