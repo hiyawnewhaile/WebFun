5bd2ed3bd925d10c1e8e90910be406632e4f6d87
Hiyawnew Haile <hiyawnewhaile@gmail.com>
2022-04-26 14:44:14 -0400

    making_connections

---
+++ W2/D2/making_connections/script.js
@@ -0,0 +1,19 @@
+function changename(){
+    var name = document.querySelector(".name")
+    name.innerText="Janet Borne"
+}
+
+// function disappears(){
+//     var request = document.querySelector("#a1")
+//     request.remove()
+// }
+
+// function disappears2(){
+//     var request = document.querySelector("#a2")
+//     request.remove()
+// }
+
+function masterdisappears(element){
+    var request = document.querySelector(element)
+    request.remove()
+}
