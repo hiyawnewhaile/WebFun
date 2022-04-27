639b4b2f9ab1e6d8f08cb6b8f94a3be5d4533280
Hiyawnew Haile <hiyawnewhaile@gmail.com>
2022-04-27 02:34:18 -0400

    dojoweatherscript

---
+++ W2/D2/dojo_weather/script.js
@@ -0,0 +1,11 @@
+
+function crumbs(){
+    var cookies = document.querySelector(".cookietime")
+    cookies.remove();
+}
+
+function f(){
+    var unit = document.querySelector(".si")
+    var celcius = sdocument.querySelector(".high")
+    document.querySelector(".high")=(high*1.8)+32
+}
