94ddcaed86fb9c877f85de959856e3dca92e250b
Hiyawnew Haile <hiyawnewhaile@gmail.com>
2022-04-27 12:30:05 -0400

    dojoweatherdone

--- W2/D2/dojo_weather/script.js
+++ W2/D2/dojo_weather/script.js
@@ -4,8 +4,20 @@ function crumbs(){
     cookies.remove();
 }
 
-function f(){
-    var unit = document.querySelector(".si")
-    var celcius = sdocument.querySelector(".high")
-    document.querySelector(".high")=(high*1.8)+32
+function f(element){
+    // var unit = document.querySelector(".si")
+    // var celcius = sdocument.querySelector(".high")
+    // document.querySelector(".high")=(high*1.8)+32
+    for(var i=1; i<5; i++){
+        var temphigh=document.querySelector(".high"+i)
+        var templow=document.querySelector(".low"+i)
+        if(element.value=="f"){
+            temphigh.innerText=Math.round((parseInt(temphigh.innerText)*1.8) + 32)
+            templow.innerText=Math.round((parseInt(templow.innerText)*1.8) + 32)
+        }
+        else{
+            temphigh.innerText=Math.round((parseInt(temphigh.innerText-32)/1.8))
+            templow.innerText=Math.round((parseInt(templow.innerText-32)/1.8))
+        }
+    }
 }
